5c516a2270dfb0af009ccddf0a29834c91f3190c
alessandrotiberia <150442623+alessandrotiberia@users.noreply.github.com>
2024-02-16 10:12:37 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Primo progetto Java");
+        Scanner sc= new Scanner(System.in);
+		System.out.println("Hello");
+		String nome = "Alessandro";
+		System.out.println("il mio nome è: "+ nome);
+        sc.close();
+    }
+}
